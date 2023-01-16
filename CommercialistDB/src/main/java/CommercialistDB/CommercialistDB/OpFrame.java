d67708f566aad9acdc80f96026f9a2546ab076cb
gekostradiwari <lukesesam94@gmail.com>
2023-01-16 13:44:06 +0100

    commit

--- CommercialistDB/src/main/java/CommercialistDB/CommercialistDB/OpFrame.java
+++ CommercialistDB/src/main/java/CommercialistDB/CommercialistDB/OpFrame.java
@@ -1,6 +1,9 @@
 package CommercialistDB.CommercialistDB;
 import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class OpFrame extends Frame {
 	String nomeStudio;
@@ -9,7 +12,17 @@ public class OpFrame extends Frame {
 		this.nomeStudio = nomeStudio;
 		JPanel panel = new JPanel(new BorderLayout());
 		panel.setSize(this.getSize());
+		JTextArea output = new JTextArea();
+		JButton op1 = new JButton("op1");
+		JButton op2 = new JButton("op2");
+		JButton op3 = new JButton("op3");
 		
+		panel.add(op1,BorderLayout.WEST);
+		panel.add(op2,BorderLayout.WEST);
+		panel.add(op3,BorderLayout.WEST);
+		panel.add(output,BorderLayout.CENTER);
+		
+		add(panel);
 	}
 	
 	
